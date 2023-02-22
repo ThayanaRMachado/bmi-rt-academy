9a6d30fbed734466d7ed182118e6e73d616a5748
ThayanaRMachado <thayanarm@hotmail.com>
2023-02-22 18:50:22 -0300

    Backend: Trainer - Exception handling.

--- backend/src/main/java/com/thayren/bmirtacademy/services/TrainerService.java
+++ backend/src/main/java/com/thayren/bmirtacademy/services/TrainerService.java
@@ -2,7 +2,11 @@ package com.thayren.bmirtacademy.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.thayren.bmirtacademy.dto.TrainerDTO;
 import com.thayren.bmirtacademy.entities.Trainer;
 import com.thayren.bmirtacademy.repositories.TrainerRepository;
+import com.thayren.bmirtacademy.services.exceptions.DatabaseException;
+import com.thayren.bmirtacademy.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class TrainerService {
@@ -29,7 +35,7 @@ public class TrainerService {
 	@Transactional(readOnly = true)
 	public TrainerDTO findById(Long id) {
 		Optional<Trainer> obj = repository.findById(id);
-		Trainer entity = obj.get();
+		Trainer entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 
 		return new TrainerDTO(entity);
 	}
@@ -45,11 +51,15 @@ public class TrainerService {
 
 	@Transactional
 	public TrainerDTO update(Long id, TrainerDTO trainerDto) {
-		Trainer entity = repository.getOne(id);
-		copyDtoToEntity(trainerDto, entity);
-		entity = repository.save(entity);
+		try {
+			Trainer entity = repository.getOne(id);
+			copyDtoToEntity(trainerDto, entity);
+			entity = repository.save(entity);
 
-		return new TrainerDTO(entity);
+			return new TrainerDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 
 	private void copyDtoToEntity(TrainerDTO trainerDto, Trainer entity) {
@@ -60,7 +70,13 @@ public class TrainerService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 }

9bae9d2980491ee60a046b4e458ba680f2dd682c
ThayanaRMachado <thayanarm@hotmail.com>
2023-02-22 16:50:32 -0300

    Backend project created

---
+++ backend/src/main/java/com/thayren/bmirtacademy/BmiRtAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.bmirtacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiRtAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiRtAcademyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayren/bmirtacademy/BmiRtAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.bmirtacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiRtAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

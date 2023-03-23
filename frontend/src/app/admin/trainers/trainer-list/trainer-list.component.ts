04c46672d88e555eeab4ab386b2187ebd2d8c416
ThayanaRMachado <thayanarm@hotmail.com>
2023-03-22 21:34:09 -0300

    Frontend: Instructor CRUD list page component layout

--- frontend/src/app/admin/trainers/trainer-list/trainer-list.component.ts
+++ frontend/src/app/admin/trainers/trainer-list/trainer-list.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainerListComponent implements OnInit {
 
+  trainers = [
+    {
+      name: 'José Sardinha',
+      age: '30',
+      cpf: '000.000.000-00',
+      cellular: '31 9 1111-1111'
+    },
+    {
+      name: 'Marcelo Zulu',
+      age: '30',
+      cpf: '000.000.000-00',
+      cellular: '31 9 1111-1111'
+    },
+    {
+      name: 'Luiz Dias',
+      age: '30',
+      cpf: '000.000.000-00',
+      cellular: '31 9 1111-1111'
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- frontend/src/app/admin/trainers/trainers.module.ts
+++ frontend/src/app/admin/trainers/trainers.module.ts
@@ -1,9 +1,14 @@
-import { TrainersRoutingModule } from './trainers-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+
+import { TrainersRoutingModule } from './trainers-routing.module';
 import { TrainerListComponent } from './trainer-list/trainer-list.component';
 import { TrainerFormComponent } from './trainer-form/trainer-form.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -14,6 +19,10 @@ import { TrainerFormComponent } from './trainer-form/trainer-form.component';
   imports: [
     CommonModule,
 
+    ButtonModule,
+    SharedModule,
+    TableModule,
+    TooltipModule,
     TrainersRoutingModule
   ],
   exports: [
